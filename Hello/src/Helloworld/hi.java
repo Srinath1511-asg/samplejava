bda61ecc6c29def363f969794ca423e5a8758fb8
sai reddy <sai reddy@DESKTOP-QIHN9J8>
2024-09-05 16:26:42 +0530

    initial commit

---
+++ Hello/src/Helloworld/hi.java
@@ -0,0 +1,7 @@
+package Helloworld;
+
+public class hi {
+public static void main(String[] args) {
+	System.out.println("Hello world");
+}
+}
---
+++ Hello/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sai reddy
+ *
+ */
+module Hello {
+}
